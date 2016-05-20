49c2c290838423f03d663ff49fcda888963c47ee
Fracastr <fr.castr@gmail.com>
2016-05-20 11:06:53 -0400

    Configuracion modelo y crud compra

--- protected/config/main.php
+++ protected/config/main.php
@@ -6,12 +6,19 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
+
+	'language'=>'es_es',
+    'sourceLanguage'=>'en',
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	
 
+
+	'aliases'=>array(
+		'bootstrap'=>realpath(__DIR__.'/../extensions/yiibooster'),
+		),
 	// preloading 'log' component
-	'preload'=>array('log'),
-
+	'preload'=>array('log','bootstrap'),
+	'theme'=>'heart',
 	// autoloading model and component classes
 	'import'=>array(
 		'application.models.*',
@@ -20,18 +27,22 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		//'resultados',
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'123',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'ipFilters'=>array('127.0.0.1','::1','201.187.37.22','190.110.163.49','186.103.158.194','190.164.184.212'),
+			'generatorPaths'=>array(
+				'ext.heart.gii'),
 		),
-		*/
+
 	),
 
 	// application components
 	'components'=>array(
+
 
 		'user'=>array(
 			// enable cookie-based authentication
@@ -73,7 +84,14 @@ return array(
 				*/
 			),
 		),
-
+		'bootstrap'=>array(
+			'class'=>'bootstrap.components.Bootstrap',
+			'fontAwesomeCss'=>true,
+			'minify'=>true,
+			),
+		'themeManager'=>array(
+			'basePath'=>'protected/extensions',
+			),
 	),
 
 	// application-level parameters that can be accessed
